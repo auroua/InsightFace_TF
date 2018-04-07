fix batch normalization error.

eval_ckpt_file.py
@@ -2,14 +2,16 @@ import tensorflow as tf
 import argparse
 from data.eval_data_reader import load_bin
 from losses.face_losses import arcface_loss
-from nets.L_Resnet_E_IR_GBN import get_resnet
+# from nets.L_Resnet_E_IR_GBN import get_resnet
+from nets.L_Resnet_E_IR import get_resnet
 import tensorlayer as tl
 from verification import ver_test
 
 
 def get_args():
     parser = argparse.ArgumentParser(description='input information')
-    parser.add_argument('--ckpt_file', default='/home/aurora/workspaces12/PycharmProjects/InsightFace_TF/output/ckpt/InsightFace_iter_',
+    # parser.add_argument('--ckpt_file', default='/home/aurora/workspaces2018/InsightFace_TF/output/ckpt2/InsightFace_iter_',
+    parser.add_argument('--ckpt_file', default='/home/aurora/workspaces2018/InsightFace_TF/output/ckpt/InsightFace_iter_best_',
                        type=str, help='the ckpt file path')
     # parser.add_argument('--eval_datasets', default=['lfw', 'cfp_ff', 'cfp_fp', 'agedb_30'], help='evluation datasets')
     parser.add_argument('--eval_datasets', default=['lfw'], help='evluation datasets')
@@ -18,9 +20,8 @@ def get_args():
     parser.add_argument('--net_depth', default=50, help='resnet depth, default is 50')
     parser.add_argument('--num_output', default=85164, help='the image size')
     parser.add_argument('--batch_size', default=32, help='batch size to train network')
-    parser.add_argument('--ckpt_index_list', default=['10000.ckpt', '20000.ckpt', '30000.ckpt', '40000.ckpt', '50000.ckpt'
-                                                      , '60000.ckpt', '70000.ckpt', '80000.ckpt', '90000.ckpt', '100000.ckpt',
-                                                      '110000.ckpt', '120000.ckpt', '130000.ckpt'], help='ckpt file indexes')
+    parser.add_argument('--ckpt_index_list',
+                        default=['982000.ckpt'], help='ckpt file indexes')
     args = parser.parse_args()
     return args
 
@@ -37,11 +38,11 @@ if __name__ == '__main__':
 
     images = tf.placeholder(name='img_inputs', shape=[None, *args.image_size, 3], dtype=tf.float32)
     labels = tf.placeholder(name='img_labels', shape=[None, ], dtype=tf.int64)
-    trainable = tf.placeholder(name='trainable_bn', dtype=tf.bool)
 
     w_init_method = tf.contrib.layers.xavier_initializer(uniform=False)
-    net = get_resnet(images, args.net_depth, type='ir', w_init=w_init_method, trainable=trainable)
+    net = get_resnet(images, args.net_depth, type='ir', w_init=w_init_method, trainable=False)
     embedding_tensor = net.outputs
+    # mv_mean = tl.layers.get_variables_with_name('resnet_v1_50/bn0/moving_mean', False, True)[0]
     # 3.2 get arcface loss
     logit = arcface_loss(embedding=net.outputs, labels=labels, w_init=w_init_method, out_num=args.num_output)
 
@@ -50,14 +51,15 @@ if __name__ == '__main__':
 
     result_index = []
     for file_index in args.ckpt_index_list:
+        feed_dict_test = {}
         path = args.ckpt_file + file_index
         saver.restore(sess, path)
         print('ckpt file %s restored!' % file_index)
-
-        feed_dict_test = {trainable: False}
         feed_dict_test.update(tl.utils.dict_to_one(net.all_drop))
+        import numpy as np
         results = ver_test(ver_list=ver_list, ver_name_list=ver_name_list, nbatch=0, sess=sess,
                            embedding_tensor=embedding_tensor, batch_size=args.batch_size, feed_dict=feed_dict_test,
                            input_placeholder=images)
         result_index.append(results)
     print(result_index)
+

nets/L_Resnet_E_IR.py
@@ -61,6 +61,12 @@ class BatchNormLayer(Layer):
 
     Batch normalization on fully-connected or convolutional maps.
 
+    ```
+        https://www.tensorflow.org/api_docs/python/tf/cond
+        If x < y, the tf.add operation will be executed and tf.square operation will not be executed.
+        Since z is needed for at least one branch of the cond, the tf.multiply operation is always executed, unconditionally.
+    ```
+
     Parameters
     -----------
     layer : a :class:`Layer` instance
@@ -84,6 +90,7 @@ class BatchNormLayer(Layer):
     ----------
     - `Source <https://github.com/ry/tensorflow-resnet/blob/master/resnet.py>`_
     - `stackoverflow <http://stackoverflow.com/questions/38312668/how-does-one-do-inference-with-batch-normalization-with-tensor-flow>`_
+
     """
 
     def __init__(
@@ -155,24 +162,11 @@ class BatchNormLayer(Layer):
             def mean_var_with_update():
                 with tf.control_dependencies([update_moving_mean, update_moving_variance]):
                     return tf.identity(mean), tf.identity(variance)
-
-            # if is_train:
-            #     mean, var = mean_var_with_update()
-            #     self.outputs = act(tf.nn.batch_normalization(self.inputs, mean, var, beta, gamma, epsilon))
-            # else:
-            #     self.outputs = act(tf.nn.batch_normalization(self.inputs, moving_mean, moving_variance, beta, gamma, epsilon))
-
-            def train_outputs():
+            if trainable:
                 mean, var = mean_var_with_update()
-                return act(tf.nn.batch_normalization(self.inputs, mean, var, beta, gamma, epsilon))
-
-            def test_outputs():
-                return act(tf.nn.batch_normalization(self.inputs, moving_mean, moving_variance, beta, gamma, epsilon))
-
-            self.outputs = tf.cond(trainable,
-                                   lambda: train_outputs(),
-                                   lambda: test_outputs())
-
+                self.outputs = act(tf.nn.batch_normalization(self.inputs, mean, var, beta, gamma, epsilon))
+            else:
+                self.outputs = act(tf.nn.batch_normalization(self.inputs, moving_mean, moving_variance, beta, gamma, epsilon))
             variables = [beta, gamma, moving_mean, moving_variance]
         self.all_layers = list(layer.all_layers)
         self.all_params = list(layer.all_params)
@@ -294,8 +288,8 @@ def bottleneck_IR_SE(inputs, depth, depth_bottleneck, stride, rate=1, w_init=Non
         return output
 
 
-def resnet(inputs, bottle_neck, blocks, w_init=None, trainable=None, scope=None):
-    with tf.variable_scope(scope):
+def resnet(inputs, bottle_neck, blocks, w_init=None, trainable=None, reuse=False, scope=None):
+    with tf.variable_scope(scope, reuse=reuse):
         # inputs = tf.subtract(inputs, 127.5)
         # inputs = tf.multiply(inputs, 0.0078125)
         net_inputs = tl.layers.InputLayer(inputs, name='input_layer')
@@ -361,7 +355,7 @@ def resnetse_v1_block(scope, base_depth, num_units, stride, rate=1, unit_fn=None
   }] * (num_units - 1))
 
 
-def get_resnet(inputs, num_layers, type=None, w_init=None, trainable=None, sess=None):
+def get_resnet(inputs, num_layers, type=None, w_init=None, trainable=None, sess=None, reuse=False):
     if type == 'ir':
         unit_fn = bottleneck_IR
     elif type == 'se_ir':
@@ -397,6 +391,7 @@ def get_resnet(inputs, num_layers, type=None, w_init=None, trainable=None, sess=
                  blocks=blocks,
                  w_init=w_init,
                  trainable=trainable,
+                 reuse=reuse,
                  scope='resnet_v1_%d' % num_layers)
     return net
 

train_nets.py
@@ -3,8 +3,8 @@ import tensorlayer as tl
 import argparse
 from data.mx2tfrecords import parse_function
 import os
-# from nets.L_Resnet_E_IR import get_resnet
-from nets.L_Resnet_E_IR_GBN import get_resnet
+from nets.L_Resnet_E_IR import get_resnet
+# from nets.L_Resnet_E_IR_GBN import get_resnet
 from losses.face_losses import arcface_loss
 from tensorflow.core.protobuf import config_pb2
 import time
@@ -31,7 +31,7 @@ def get_parser():
     parser.add_argument('--ckpt_path', default='./output/ckpt', help='the ckpt file save path')
     parser.add_argument('--log_file_path', default='./output/logs', help='the ckpt file save path')
     parser.add_argument('--saver_maxkeep', default=100, help='tf.train.Saver max keep ckpt files')
-    parser.add_argument('--buffer_size', default=2000, help='tf dataset api buffer size')
+    parser.add_argument('--buffer_size', default=10000, help='tf dataset api buffer size')
     parser.add_argument('--log_device_mapping', default=False, help='show device placement log')
     parser.add_argument('--summary_interval', default=300, help='interval to save summary')
     parser.add_argument('--ckpt_interval', default=10000, help='intervals to save ckpt file')
@@ -72,10 +72,14 @@ if __name__ == '__main__':
     # 3. define network, loss, optimize method, learning rate schedule, summary writer, saver
     # 3.1 inference phase
     w_init_method = tf.contrib.layers.xavier_initializer(uniform=False)
-    net = get_resnet(images, args.net_depth, type='ir', w_init=w_init_method, trainable=trainable)
-    embedding_tensor = net.outputs
+    net = get_resnet(images, args.net_depth, type='ir', w_init=w_init_method, trainable=True)
     # 3.2 get arcface loss
     logit = arcface_loss(embedding=net.outputs, labels=labels, w_init=w_init_method, out_num=args.num_output)
+    # test net  because of batch normal layer
+    tl.layers.set_name_reuse(True)
+    test_net = get_resnet(images, args.net_depth, type='ir', w_init=w_init_method, trainable=False, reuse=True)
+    embedding_tensor = test_net.outputs
+
     # 3.3 define the cross entropy
     inference_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logit, labels=labels))
     # inference_loss_avg = tf.reduce_mean(inference_loss)
@@ -144,12 +148,15 @@ if __name__ == '__main__':
     # 3.13 init all variables
     sess.run(tf.global_variables_initializer())
 
+    restore_saver = tf.train.Saver()
+    restore_saver.restore(sess, '/home/aurora/workspaces2018/InsightFace_TF/output/ckpt/InsightFace_iter_best_1026000.ckpt')
+    # 4 begin iteration
     if not os.path.exists(args.log_file_path):
         os.makedirs(args.log_file_path)
     log_file_path = args.log_file_path + '/train' + time.strftime('_%Y-%m-%d-%H-%M', time.localtime(time.time())) + '.log'
     log_file = open(log_file_path, 'w')
     # 4 begin iteration
-    count = 0
+    count = 1026000
     total_accuracy = {}
     for i in range(args.epoch):
         sess.run(iterator.initializer)
@@ -192,23 +199,24 @@ if __name__ == '__main__':
                     results = ver_test(ver_list=ver_list, ver_name_list=ver_name_list, nbatch=count, sess=sess,
                              embedding_tensor=embedding_tensor, batch_size=args.batch_size, feed_dict=feed_dict_test,
                              input_placeholder=images)
+                    print('test accuracy is: ', str(results[0]))
                     total_accuracy[str(count)] = results[0]
-                    print('########'*30)
-                    print(list(total_accuracy.keys()))
-                    print(list(total_accuracy.values()))
-                    log_file.write('########'*30+'\n')
+                    log_file.write('########'*10+'\n')
                     log_file.write(','.join(list(total_accuracy.keys())) + '\n')
-                    log_file.write(','.join([str(val) for val in list(total_accuracy.values())]) + '\n')
+                    log_file.write(','.join([str(val) for val in list(total_accuracy.values())])+'\n')
                     log_file.flush()
                     if max(results) > 0.996:
                         print('best accuracy is %.5f' % max(results))
                         filename = 'InsightFace_iter_best_{:d}'.format(count) + '.ckpt'
                         filename = os.path.join(args.ckpt_path, filename)
                         saver.save(sess, filename)
-                        log_file.write('####Best Accuracy####')
+                        log_file.write('######Best Accuracy######'+'\n')
                         log_file.write(str(max(results))+'\n')
                         log_file.write(filename+'\n')
+
+                        log_file.flush()
             except tf.errors.OutOfRangeError:
                 print("End of epoch %d" % i)
                 break
     log_file.close()
+    log_file.write('\n')